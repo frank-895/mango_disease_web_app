9751ab8b558ef1bd4ab7f29dc48d04d9a9f10ed4
Frank <frank.snelling03@gmail.com>
2025-03-29 19:17:09 +0000

    added basic functions to view.py for rendering and added main pages to urls.py

--- mango_disease_proj/mango_disease_app/views.py
+++ mango_disease_proj/mango_disease_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'mango_disease_app/home.html')
+
+def diseases(request):
+    return render(request, 'mango_disease_app/diseases.html')
+
+def about(request):
+    return render(request, 'mango_disease_app/about.html')
--- mango_disease_proj/mango_disease_proj/settings.py
+++ mango_disease_proj/mango_disease_proj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Apps
+    'mango_disease_app',
 ]
 
 MIDDLEWARE = [
