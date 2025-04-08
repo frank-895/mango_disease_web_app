401ace212f4bf932ed738ad740bc6036a4bbce81
Frank <frank.snelling03@gmail.com>
2025-04-08 10:03:00 +0100

    Personality Traits added to Author Card

--- mango_disease_proj/mango_disease_app/views.py
+++ mango_disease_proj/mango_disease_app/views.py
@@ -9,5 +9,13 @@ def diseases(request):
 
 def about(request):
     cards = AuthorCard.objects.all()
+    
+    # Scale the personality traits to a 100 scale (for percentage)
+    for card in cards:
+        card.collaborator_independent = int(card.collaborator_independent) * 10
+        card.bigPicture_detailOriented = int(card.bigPicture_detailOriented) * 10
+        card.communicator_listener = int(card.communicator_listener) * 10
+        card.creative_practical = int(card.creative_practical) * 10
+    
     page_data = {'cards': cards}
     return render(request, 'mango_disease_app/about.html', page_data)
