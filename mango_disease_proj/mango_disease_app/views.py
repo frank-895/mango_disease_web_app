831e3900f143aa8911e1b40ec96ed12f2e138e38
michael-lewis1 <S342724@students.cdu.edu.au>
2025-04-15 17:26:09 +0930

    Pfp for about... I dont have anything else

--- mango_disease_proj/mango_disease_app/views.py
+++ mango_disease_proj/mango_disease_app/views.py
@@ -21,7 +21,8 @@ michael = AuthorCard("Michael Lewis",
                    50,
                    80,
                    60,
-                   70)
+                   70,
+                   "Michael.jpg")
 
 opeoluwa = AuthorCard("Opeoluwa Adebiyi",
                       "S373330",
@@ -56,7 +57,7 @@ disease1 = data_model("Mango Pink Scale",True,4,6,
                       "Due to Mango scale being a threat throughout the growth cycle, frequent monitoring is recommended every fortnight, inspecting leaves and twigs less than 1 year old,  5 Branches on 10 tress per hectare. Pyriproxyfen is the recommended sprayed, targeted on the insects locations and additionally throughout the branches, fruit and foliage. No more than 2 applications per season with 4 weeks between.",
                       'Mangoscale.png')
 disease2 = data_model("Bacterial Black Spot",False,4,4,
-                      "A fungal infection that is common in southern areas of Australia or where there has been unseasonal wet conditions. ",
+                      "A fungal infection that is common in southern areas of Australia or where there has been unseasonal wet conditions.",
                       "Bacterial Black Spot is caused by the fungus Xanthomonas campestris pv. mangiferae-indicae and causes black scabby spots to form on leaves. These spots commonly develop into secondary rots and futher form into deep fruit decay. In extreme cases fruit with Bacterial Black Spot can become unmarketable. ",
                       "The bacteria can lie dormant on stem lesions and as the mango trees develop it then spreads to the leaves and fruit; it can then further spread from tree to tree in a field by strong winds in rain or tools used for management. Copper Oxychloride spray recommends for treatment every 4 weeks during the flowering to fruit stages. To reduce the risk of initial infection it is also recommended to frequently clean pruning and harvesting equipment or plant resistant fruit.",
                       'BlackSpot.png')
