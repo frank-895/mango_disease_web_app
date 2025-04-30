e71b33905c364312562d811e6216849bf252f6a7
Patrick-Dunn-CDU <S378262@students.cdu.edu.au>
2025-04-30 18:13:15 +1000

    Created beginning of the django database models using ERD_final as a structure point

--- mango_disease_proj/mango_disease_app/admin.py
+++ mango_disease_proj/mango_disease_app/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+import mango_disease_app.models as models
+#from mango_disease_app.models import User, Variety, Location, Orchard, Disease, Record, LocationDisease, VarietyDiseaseSusceptible, OrchardUser, RecordDisease
+
+# Model Registration
+admin.site.register(models.User)
+admin.site.register(models.Variety)
+admin.site.register(models.Location)
+admin.site.register(models.Orchard)
+admin.site.register(models.Disease)
+admin.site.register(models.Record)
+admin.site.register(models.LocationDisease)
+admin.site.register(models.VarietyDiseaseSusceptible)
+admin.site.register(models.OrchardUser)
+admin.site.register(models.RecordDisease)
--- mango_disease_proj/mango_disease_app/models.py
+++ mango_disease_proj/mango_disease_app/models.py
@@ -1,1 +1,94 @@
 from django.db import models
+from django.core import validators
+from django.utils import timezone
+
+class User(models.Model):
+    userID = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField()
+
+class Variety(models.Model):
+    varietyID = models.AutoField(primary_key=True)
+
+class Location(models.Model):
+    locationID = models.AutoField(primary_key=True)
+    locationName = models.CharField()
+
+class Orchard(models.Model):
+    orchardID = models.AutoField(primary_key=True)
+    orchardName = models.CharField()
+    noTreesRow = models.IntegerField()
+    noTreesColumn = models.IntegerField()
+    varietyID = models.ForeignKey(
+        Variety,
+        on_delete=models.PROTECT
+    )
+    locationID = models.ForeignKey(
+        Location,
+        on_delete=models.PROTECT
+    )
+
+class Disease(models.Model):
+    diseaseID = models.AutoField(primary_key=True)
+    diseaseName = models.CharField()
+    severity = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
+    spreadability = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
+    shortDescription = models.CharField()
+    longDescription = models.CharField()
+    controlMethod = models.CharField()
+
+plantParts = {
+    'leaf':'Leaf',
+    'stem':'Stem',
+    'fruit':'Fruit',
+}
+class Record(models.Model):
+    recordID = models.AutoField(primary_key=True)
+    orchardID = models.ForeignKey(
+        Orchard,
+        on_delete=models.PROTECT
+    )
+    recordedAt = models.DateTimeField(default=timezone.now)
+    partOfPlant = models.CharField(choices=plantParts)
+
+
+class LocationDisease(models.Model):
+    locationID = models.ForeignKey(
+        Location,
+        on_delete=models.PROTECT
+    )
+    diseaseID = models.ForeignKey(
+        Disease,
+        on_delete=models.PROTECT
+    )
+    susecptability = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
+
+class VarietyDiseaseSusceptible(models.Model):
+    varietyID = models.ForeignKey(
+        Variety,
+        on_delete=models.PROTECT
+    )
+    diseaseID = models.ForeignKey(
+        Disease,
+        on_delete=models.PROTECT
+    )
+
+class OrchardUser(models.Model):
+    orchardID = models.ForeignKey(
+        Orchard,
+        on_delete=models.PROTECT
+    )
+    userID = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT
+    )
+
+class RecordDisease(models.Model):
+    recordID = models.ForeignKey(
+        Record,
+        on_delete=models.PROTECT
+    )
+    diseaseID = models.ForeignKey(
+        Disease,
+        on_delete=models.PROTECT
+    )
