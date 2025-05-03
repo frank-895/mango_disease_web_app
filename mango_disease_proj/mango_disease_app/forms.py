264a0d25754028c1d2d20f46b344429cc5f00131
Frank <frank.snelling03@gmail.com>
2025-05-03 14:14:06 +0100

    Final Alterations to model.py

--- mango_disease_proj/mango_disease_app/forms.py
+++ mango_disease_proj/mango_disease_app/forms.py
@@ -5,3 +5,8 @@ class addDiseaseRecord(forms.ModelForm):
     class Meta:
         model = Record
         exclude = []
+        labels = {
+            'orchardId':"TEMPORARY - needs to be changed to whoever is logged in",
+            'recordedAt': "Enter time of recording: ",
+            'partOfPlant': "Choose which part of plant was impacted: ",
+        }
--- mango_disease_proj/mango_disease_app/models.py
+++ mango_disease_proj/mango_disease_app/models.py
@@ -4,91 +4,90 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Variety(models.Model):
-    varietyID = models.AutoField(primary_key=True)
-    varietyName = models.CharField()
+    varietyName = models.CharField(max_length=255)
     
     def __str__(self):
         return self.varietyName
 
 class Location(models.Model):
-    locationID = models.AutoField(primary_key=True)
-    locationName = models.CharField()
+    locationName = models.CharField(max_length=255)
     
     def __str__(self):
         return self.locationName
 
 class Orchard(models.Model):
-    orchardID = models.AutoField(primary_key=True)
-    orchardName = models.CharField()
+    orchardName = models.CharField(max_length=255)
     noTreesRow = models.IntegerField()
     noTreesColumn = models.IntegerField()
-    varietyID = models.ForeignKey(
+    variety = models.ForeignKey(
         Variety,
         on_delete=models.PROTECT
     )
-    locationID = models.ForeignKey(
+    location = models.ForeignKey(
         Location,
         on_delete=models.PROTECT
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,  # If a user is deleted, delete the orchard too
-        related_name='orchards',
-        null=True, blank=True
     )
     
     def __str__(self):
         return self.orchardName
 
 class Disease(models.Model):
-    diseaseID = models.AutoField(primary_key=True)
-    diseaseName = models.CharField()
+    diseaseName = models.CharField(max_length=255)
     severity = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
     spreadability = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
-    shortDescription = models.CharField()
-    longDescription = models.CharField()
-    controlMethod = models.CharField()
+    shortDescription = models.TextField()
+    longDescription = models.TextField()
+    controlMethod = models.TextField()
     
     def __str__(self):
         return self.diseaseName
 
-plantParts = {
-    'leaf':'Leaf',
-    'stem':'Stem',
-    'fruit':'Fruit',
-}
-
 class Record(models.Model):
-    recordID = models.AutoField(primary_key=True)
+    PLANT_PARTS = [
+        ('leaf', 'Leaf'),
+        ('stem', 'Stem'),
+        ('fruit', 'Fruit'),
+    ]
+    
     orchardID = models.ForeignKey(
         Orchard,
-        on_delete=models.PROTECT
+        on_delete=models.CASCADE
     )
     recordedAt = models.DateTimeField(default=timezone.now)
-    partOfPlant = models.CharField(choices=plantParts)
+    partOfPlant = models.CharField(choices=PLANT_PARTS, max_length=5)
     disease = models.ForeignKey(Disease, related_name='records', on_delete=models.PROTECT)
     
     def __str__(self):
-        return f"{self.orchardID} - Disease: {self.disease}"
+        return f"Record for {self.orchardID} - Disease: {self.disease} at {self.recordedAt}"
     
 class LocationDisease(models.Model):
-    locationID = models.ForeignKey(
+    location = models.ForeignKey(
         Location,
-        on_delete=models.PROTECT
+        on_delete=models.CASCADE
     )
-    diseaseID = models.ForeignKey(
+    disease = models.ForeignKey(
         Disease,
-        on_delete=models.PROTECT
+        on_delete=models.CASCADE
     )
     locationSusceptability = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
+    
+    def __str__(self):
+        return f"{self.location} susceptibility to {self.disease}"
 
 class VarietyDisease(models.Model):
-    varietyID = models.ForeignKey(
+    variety = models.ForeignKey(
         Variety,
-        on_delete=models.PROTECT
+        on_delete=models.CASCADE
     )
-    diseaseID = models.ForeignKey(
+    disease = models.ForeignKey(
         Disease,
-        on_delete=models.PROTECT
+        on_delete=models.CASCADE
     )
     varietySusceptability = models.IntegerField(validators=[validators.MinValueValidator(0),validators.MaxValueValidator(10)])
+    
+    def __str__(self):
+        return f"{self.variety} susceptibility to {self.disease}"
