c06c46b09ed69e00b3c3153c830c6f16e26dca77
Frank <frank.snelling03@gmail.com>
2025-05-15 12:45:16 +0100

    UPdate setings.py for production

--- mango_disease_proj/mango_disease_proj/settings.py
+++ mango_disease_proj/mango_disease_proj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vz87vf643uq3gp%-8i)q+l=n51n%z^+639ya1o3)k5vark@yf-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['franksnelling.pythonanywhere.com']
 
 
 # Application definition
