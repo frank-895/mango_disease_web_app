f1f451f15770a60876c31afa3ac7a0d976f03418
Frank <frank.snelling03@gmail.com>
2025-05-15 12:42:57 +0100

    defined static_root for production

--- mango_disease_proj/mango_disease_proj/settings.py
+++ mango_disease_proj/mango_disease_proj/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
