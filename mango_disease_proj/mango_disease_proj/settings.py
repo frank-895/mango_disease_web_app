86addab536f37cc5e97b641d64fb093e080d9504
Frank <frank.snelling03@gmail.com>
2025-05-15 13:47:36 +0100

    Update settings.py

--- mango_disease_proj/mango_disease_proj/settings.py
+++ mango_disease_proj/mango_disease_proj/settings.py
@@ -34,6 +34,11 @@ else:
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mango_disease_app', 'static'), 
+]
 
 # Application definition
 
@@ -123,18 +128,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = '/static/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mango_disease_app', 'static'), 
-]
 
 
 # Default primary key field type
